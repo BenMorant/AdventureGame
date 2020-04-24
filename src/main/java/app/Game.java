2db99a528b012a9e4803f88d1ea83a4a80947caf
bmorant <b.morant@lefebvre-sarrut.eu>
2020-04-24 21:03:27 +0200

    making some implementations/extensions : first, used import java.util.Observer and this tutorial : https://perso.telecom-paristech.fr/hudry/coursJava/interSwing/boutons5.html - but it has been deprecated since Java 9.
    So I'm trying to use java.beans.PropertyChangeEvent;
                         and java.beans.PropertyChangeListener;
    
                 look :        https://gist.github.com/mtorchiano/e69ac7e309fee81bd17f4f0740b9ffa9

--- src/main/java/app/Game.java
+++ src/main/java/app/Game.java
@@ -19,7 +19,7 @@ public final class Game implements Runnable {
         Controller controller = new Controller(screen, gui);
         screen.addObserver(gui);
         screen.setProperty("new");
-  
+
     }
 
     @Override
