1ff369fafd3e5ad303b4d28289ed050e942c7620
bmorant <b.morant@lefebvre-sarrut.eu>
2020-04-28 08:36:27 +0200

    created GameFrame

--- src/main/java/view/GUI.java
+++ src/main/java/view/GUI.java
@@ -5,32 +5,57 @@ import observer.Observer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI extends JFrame implements Observer {
+public class GUI extends GameFrame implements Observer {
 
-    public static final int DEFAULT_WIDTH = 1280;
-    public static final int DEFAULT_HEIGHT = DEFAULT_WIDTH / 12 * 9;
+//    public static final int DEFAULT_WIDTH = 1280;
+//    public static final int DEFAULT_HEIGHT = DEFAULT_WIDTH / 12 * 9;
 
     private JPanel container = new JPanel();
+    private JButton startButton = new JButton("start");
+    private JPanel titlePanel = new JPanel();
+    private JPanel mainPanel = new JPanel();
+    private CardLayout cardLayout = new CardLayout();
+
+
 
     private AbstractController controller;
 
     public GUI(AbstractController controller) {
-        this.setMinimumSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
-        this.setPreferredSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
-        this.setMaximumSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
-        this.setTitle("titre");
-        this.setResizable(false);
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
+//        this.setMinimumSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+//        this.setPreferredSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+//        this.setMaximumSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+//        this.setTitle("titre");
+//        this.setResizable(false);
+//        this.setLocationRelativeTo(null);
+//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        this.setLayout(null);
         this.controller = controller;
         this.setContentPane(container);
-        this.getContentPane().setBackground(Color.black);
+        container.setBackground(Color.black);
+        container.setLayout(cardLayout);
+        titlePanel.add(startButton);
+        titlePanel.setBackground(Color.green);
+        mainPanel.setBackground(Color.pink);
+        container.add(titlePanel, "1");
+        container.add(mainPanel, "2");
+        cardLayout.show(container, "1");
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                cardLayout.show(container, "2");
+            }
+        });
+
         setVisible(true);
         pack();
 
     }
+
+
+
 
     @Override
     public void update(String string) {
---
+++ src/main/java/view/GameFrame.java
@@ -0,0 +1,26 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+    public class GameFrame extends JFrame {
+
+        public static final int DEFAULT_WIDTH = 1280;
+        public static final int DEFAULT_HEIGHT = DEFAULT_WIDTH / 12 * 9;
+        public static final String TITLE = "L'aventure n'attend pas";
+        private static final Color FRAME_COLOR = Color.black;
+
+
+        public GameFrame() {
+            super(TITLE);
+            this.setMinimumSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+            this.setPreferredSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+            this.setMaximumSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+            this.setResizable(false);
+            this.setLocationRelativeTo(null);
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.getContentPane().setBackground(FRAME_COLOR);
+            pack();
+        }
+    }
+
