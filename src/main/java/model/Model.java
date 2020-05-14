d1c4f6c0190504135f6702dae4d607400e9fa784
bmorant <b.morant@lefebvre-sarrut.eu>
2020-05-14 15:19:19 +0200

    deleted StoryBlock and AbstractModel .

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,14 +1,21 @@
 package model;
 
 
-public class Model extends AbstractModel {
+import observer.Observable;
+import observer.Observer;
+
+import java.util.ArrayList;
+
+public class Model implements Observable {
 
     private People hero = new People(13, "couteau");
     private People goblin = new People(20, "baton");
     private boolean hasSilverRing = false;
 
     private String position;
-    
+
+    private ArrayList<Observer> observers = new ArrayList<>();
+
     public Model() {
         this.position = null;
 
@@ -23,4 +30,15 @@ public class Model extends AbstractModel {
     }
 
 
+    @Override
+    public void addObserver(Observer observer) {
+        this.observers.add(observer);
+    }
+
+    @Override
+    public void notifyHero(People newValue) {
+        for (Observer observer : observers) {
+            observer.updateHero(hero);
+        }
+    }
 }
